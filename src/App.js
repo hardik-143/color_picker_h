28828a520ee2f45594fefce36efc496f83e610a0
Hardik <dhardik1430@gmail.com>
2023-12-21 09:15:25 +0530

    updated wrapper width

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         }}
       >
         <Toaster position="bottom-center" reverseOrder={false} />
-        <section className="w-full max-w-[500px] py-5">
+        <section className="w-[90%] max-w-[500px] py-5">
           <div className="text-black bg-white p-4 rounded-xl shadow-[0px_4px_15px_rgba(150,150,150,0.32)] relative z-10">
             <PickerBody />
 
